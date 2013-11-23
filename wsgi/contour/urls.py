d477b42bd348ef246b70735f3fe0b59393d2a1d5
Hiroyuki Sakai <HiroyukiSakai@users.noreply.github.com>
2013-11-24 00:45:36 +0100

    Serving icons, robots.txt, etc.

--- wsgi/contour/urls.py
+++ wsgi/contour/urls.py
@@ -14,4 +14,11 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^crossdomain\.xml$', 'django.views.generic.simple.direct_to_template', {'template': 'crossdomain.xml', 'mimetype': 'application/xml'}),
+    url(r'^humans\.txt$', 'django.views.generic.simple.direct_to_template', {'template': 'humans.txt', 'mimetype': 'text/plain'}),
+    url(r'^robots\.txt$', 'django.views.generic.simple.direct_to_template', {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+
+    url(r'^apple-touch-icon-precomposed\.png$', 'django.views.generic.simple.redirect_to', {'url': '/static/apple-touch-icon-precomposed.png'}),
+    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.ico'}),
 )
