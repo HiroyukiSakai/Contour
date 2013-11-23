36901e8c0e3ace5e70f8ab363b2f464d00da1324
Hiroyuki Sakai <HiroyukiSakai@users.noreply.github.com>
2013-11-23 23:45:20 +0100

    Configuration debugging

--- wsgi/contour/settings.py
+++ wsgi/contour/settings.py
@@ -1,17 +1,6 @@
 # -*- coding: utf-8 -*-
-"""
-Django settings for contour project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
+# Django settings for openshift project.
 import imp, os
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # a setting to determine whether we are running on OpenShift
 ON_OPENSHIFT = False
@@ -64,7 +53,7 @@ else:
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -79,8 +68,6 @@ USE_I18N = True
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
 USE_L10N = True
-
-USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
