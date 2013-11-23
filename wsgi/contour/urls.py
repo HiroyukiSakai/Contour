0e4994bc8b1ff2e936e5b9765e09d068ce907556
Hiroyuki Sakai <HiroyukiSakai@users.noreply.github.com>
2013-11-23 23:58:58 +0100

    Template update

--- wsgi/contour/urls.py
+++ wsgi/contour/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'contour.views.home', name='home'),
+    url(r'^$', 'contour.views.index', name='index'),
     # url(r'^openshift/', include('openshift.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
