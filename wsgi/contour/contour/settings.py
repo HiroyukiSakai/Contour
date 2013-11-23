351aa28090fc281873edd2b1c9aab6965a386ea3
Hiroyuki Sakai <HiroyukiSakai@users.noreply.github.com>
2013-11-23 22:47:41 +0100

    Configuration debugging

--- wsgi/contour/contour/settings.py
+++ wsgi/contour/contour/settings.py
@@ -147,7 +147,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'contour.urls'
