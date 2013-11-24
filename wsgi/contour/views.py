e4a3bb95f1bec00ce960cb1935027900895a3538
Hiroyuki Sakai <HiroyukiSakai@users.noreply.github.com>
2013-11-24 12:03:38 +0100

    Trying to install PIL

--- wsgi/contour/views.py
+++ wsgi/contour/views.py
@@ -1,7 +1,7 @@
 import os
 from django.shortcuts import render_to_response
-from PIL import Image
+#from PIL import Image
 
 def index(request):
-    src_im = Image.open("winter3.jpg")
+    #src_im = Image.open("winter3.jpg")
     return render_to_response('index.html')
