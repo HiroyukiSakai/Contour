22a241725b18c50adc120bd66a8dfc3ed03a3890
Hiroyuki Sakai <HiroyukiSakai@users.noreply.github.com>
2013-12-11 17:24:21 +0100

    PIL test

--- wsgi/contour/views.py
+++ wsgi/contour/views.py
@@ -2,5 +2,5 @@ import os, Image
 from django.shortcuts import render_to_response
 
 def index(request):
-    src_im = Image.open(os.getenv('OPENSHIFT_DATA_DIR').join("lenna.png"))
+    src_im = Image.open(os.path.join(os.getenv('OPENSHIFT_DATA_DIR'), "lenna.png"))
     return render_to_response('index.html')
