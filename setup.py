f73425307648cf8785854f83a35d251b7be203f8
Hiroyuki Sakai <HiroyukiSakai@users.noreply.github.com>
2013-12-15 18:24:02 +0100

    Changed requirements order

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setup(
     author_email='hiroyuki.sakai@alumni.tuwien.ac.at',
     url='http://contour-sakai.rhcloud.com/',
     install_requires=[
-        'Cython==0.19.2',
         'Django==1.6.1',
         'Pillow==2.2.1',
         'numpy==1.8.0',
         'scipy==0.12.1',
-        'scikit-image==0.9.3'
+        'scikit-image==0.9.3',
+        'Cython==0.19.2',
     ],
 )
