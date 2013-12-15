cd3c5f0efcff24993756a0c0b23915b493c33a83
Hiroyuki Sakai <HiroyukiSakai@users.noreply.github.com>
2013-12-15 19:04:35 +0100

    OpenShift installation

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     #    'scikit-image==0.9.3',
     #    'scipy==0.12.1',
     #    'numpy==1.8.0',
-    #    'Pillow==2.2.1',
+        'Pillow==2.2.1',
         'Django==1.6.1',
         'Cython==0.19.2', # this is needed for the scikit-image package installation
     ],
