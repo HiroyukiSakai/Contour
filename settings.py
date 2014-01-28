06912cbe1588fb75f32dc27452a59252cf72ddc6
Hiroyuki Sakai <HiroyukiSakai@users.noreply.github.com>
2014-01-28 01:09:41 +0100

    Static files

--- settings.py
+++ settings.py
@@ -49,9 +49,11 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/Hiro/Contour/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -62,7 +64,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/Hiro/Contour/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -112,6 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'contour/templates'),
 )
 
 INSTALLED_APPS = (
