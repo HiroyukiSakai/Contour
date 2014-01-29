0be2c65735a4b97e22675c4bb33bfa34889591fb
Hiroyuki Sakai <HiroyukiSakai@users.noreply.github.com>
2014-01-29 13:05:12 +0000

    Template updates

--- contour/static/contour/js/main.js
+++ contour/static/contour/js/main.js
@@ -1,6 +1,42 @@
 $(function() {
-    $("input:file").change(function (){
-        var fileName = $(this).val();
-        $(".filename").html(fileName);
+    // you may want to disable scrolling on touch devices
+    $(document).bind('touchmove', function(e) {
+        if (e.target === document.documentElement) {
+            return e.preventDefault();
+        }
     });
+
+    // http://davidwalsh.name/fullscreen
+    // Find the right method, call on correct element
+    function launchFullScreen(element) {
+        if(element.requestFullScreen) {
+            element.requestFullScreen();
+        } else if(element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if(element.webkitRequestFullScreen) {
+            element.webkitRequestFullScreen();
+        }
+    }
+
+    $('#fullscreen-test').click(function() {
+        launchFullScreen(document.documentElement); // any individual element
+    });
+
+    $('.view-main-menu #fileinput').on('change', function(evt) {
+        var file = evt.target.files[0];
+        if (file) {
+            $('#upload-btn').show();
+        } else {
+            $('#upload-btn').hide();
+        }
+    });
+
+    if ('literallycanvas' in $) {
+        // the only LC-specific thing we have to do
+        $('.view-game .literally').literallycanvas({
+            backgroundColor: '#ffffff',
+            imageURLPrefix: '{{ STATIC_URL }}contour/vendor/literallycanvas/img',
+            keyboardShortcuts: false
+        });
+    }
 });
